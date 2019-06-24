28c984a6e4402ed0b88012104b4acfc7f48062e3
Bren <captainbmq@gmail.com>
2019-06-24 10:02:46 -0400

    semi functioning OOP example

---
+++ OOJ.js
@@ -0,0 +1,36 @@
+// create a function that console logs any parameters given
+function funLog(content) {
+    console.log(content)
+}
+
+// create a function that takes a name/string as a parameter
+// and returns "hi my name is" parameter given
+
+function speak(name) {
+    return "Hi, my name is " + name + "."
+}
+
+funLog(speak("jimmy"))
+
+
+
+
+
+
+
+// create 3 functions that descripe the powers of a ninja you would like to create
+
+function firstP(fAttribute) {
+    return "Your ninja wears " + fAttribute + "."
+}
+function secondP(sAttribute) {
+    return "Your ninja is very" + sAttribute + "."
+}
+function thirdP(tAttribute) {
+    return "Your ninja is really just a " + tAttribute + "."
+}
+
+display(name("William"));
+display(firstp("Full Plate Armor"));
+display(secondP("Chivalrous"));
+display(thirdP("Knight"))
